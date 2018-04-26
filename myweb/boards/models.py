58dc1550f1a382112b90bd849375a4fb4d6e0f9c
colin <ttbuge@gmail.com>
2018-04-26 16:25:59 +0800

    add some new feature like markdown and fix some questions

--- myweb/boards/models.py
+++ myweb/boards/models.py
@@ -1,4 +1,7 @@
+import math
 from django.db import models
+from django.utils.html import mark_safe
+from markdown import markdown
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
@@ -28,6 +31,24 @@ class Topic(models.Model):
     def __str__(self):
         return self.subject
 
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1,5)
+        return range(1, count+1)
+    
+    def get_last_ten_posts(self):
+        return self.posts.order_by('-created_at')[:10]
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -36,6 +57,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
 
     # show first 30 chars
     def __str__(self):
